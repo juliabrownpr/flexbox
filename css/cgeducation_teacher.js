0bb4f53a146d694409348efc8814d6ce5fcf0986
BROWN <JBROW10@monsanto.com>
2017-09-27 19:24:10 -0500

    adding javascript

---
+++ css/cgeducation_teacher.js
@@ -0,0 +1,12 @@
+console.log("This file is linked!");
+var teacherName = "Mr. Paint";
+var department = "Art";
+var rating = ["2.3", "1.2", "0.5"];
+
+console.log("Teacher: " + teacherName);
+console.log("Department: " + department);
+console.log("Ratings: " + rating);
+
+var avg = (2.3+1.2+.5) / (3);
+var aveRating = Math.round(avg * 100) / 100;
+console.log("Avg Rating: " + aveRating);
