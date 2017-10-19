cab3eea919c9817f98e8ae21706f4aab5964e243
BROWN <JBROW10@monsanto.com>
2017-10-18 19:27:09 -0500

    completed js for students

--- css/cgeducation_students.js
+++ css/cgeducation_students.js
@@ -1,1 +1,10 @@
 console.log("This file is linked!");
+var studentname = "Wilhelmina Gates";
+var major = "Computer Science";
+var email = "wilhelmina@cg.edu";
+var gpa = "4.5";
+var course = "Retro Tech: Creating New Tech that Feels Old";
+
+console.log(studentname + " is majoring in " + major + " and is currently enrolled in " + course + ".");
+console.log("Her email is " + email + ".");
+console.log("Her GPA is " + gpa + ".");
