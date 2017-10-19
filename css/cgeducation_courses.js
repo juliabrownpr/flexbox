a036efd2a51bc09c518a293b276eb09149ac9e1d
BROWN <JBROW10@monsanto.com>
2017-10-18 19:38:24 -0500

    added javascript for courses

---
+++ css/cgeducation_courses.js
@@ -0,0 +1,7 @@
+console.log("This file is linked!")
+var courses = ["Lifelike Pinatas", "Basics of Finger Painting", "It's All Fun and Games Until Van Gogh Loses an Ear"]
+var teacher = "Mr. Paint";
+var semester = "Spring 2018";
+
+console.log(teacher + " is teaching " + courses + ".");
+console.log(teacher + " is teaching these courses in the " + semester + " semester.");
