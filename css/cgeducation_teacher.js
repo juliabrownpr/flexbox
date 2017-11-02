79d0feb9eec2f5c50ca274ad759af78971a3ea15
BROWN <JBROW10@monsanto.com>
2017-11-01 19:54:53 -0500

    fixed javascript 1 challenge

--- css/cgeducation_teacher.js
+++ css/cgeducation_teacher.js
@@ -1,12 +1,20 @@
 console.log("This file is linked!");
 var teacherName = "Mr. Paint";
 var department = "Art";
-var rating = ["2.3", "1.2", "0.5"];
+var rating = [2.3, 1.2, 0.5];
 
 console.log("Teacher: " + teacherName);
 console.log("Department: " + department);
 console.log("Ratings: " + rating);
 
-var avg = (2.3+1.2+.5) / (3);
-var aveRating = Math.round(avg * 100) / 100;
-console.log("Avg Rating: " + aveRating);
+var sum = 0;
+for (var i=0; i<rating.length; i++){
+  sum += rating[i];
+  console.log(sum);
+}
+
+var avg = sum / rating.length;
+console.log(avg);
+
+avg = Math.round(avg * 100) / 100;
+console.log(avg);
