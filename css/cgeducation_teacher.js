9e1d19422d3d042ed54df2fb76c3cb1f847ab896
BROWN <JBROW10@monsanto.com>
2017-12-13 19:49:09 -0600

    fixed a whole bunch! line 24 isn't working

--- css/cgeducation_teacher.js
+++ css/cgeducation_teacher.js
@@ -1,27 +1,27 @@
 console.log("This file is linked!");
 var teacherName = "Mr. Paint";
 var department = "Art";
-var rating = [2.3, 1.2, 0.5];
+var teacherRating = [2.3, 1.2, 0.5];
+
+function addTeacherRating (teacherRating, newRating) {
+  teacherRating.unshift(newRating);
+  return teacherRating;
+}
 
 function getAvgRating (rating){
   var sum = 0;
-  for (var i=0; i<rating.length; i++){
-    sum += rating[i];
+  for (var i=0; i<addTeacherRating.length; i++){
+    sum += addTeacherRating[i];
   }
-  var avg = sum / rating.length;
+  var avg = sum / addTeacherRating.length;
   avg = Math.round(avg * 100) / 100;
   return avg;
 }
 
-function addTeacherRating (rating, newRating) {
-  rating.push(newRating);
-  return rating;
-}
-
-var newRating = prompt("We would like for you to review our teacher. Please enter a rating between 0.0 and 5.0?");
-  if (newRating >=0 && <=5) {
-    addTeacherRating;
-    alert("Thanks for your review! " + teacherName + "average rating is now " + getAvgRating + ".");
+var newRating = parseInt(prompt("We would like for you to review our teacher. Please enter a rating between 0.0 and 5.0?"));
+  if (newRating >=0 && newRating <=5) {
+    console.log("Too many chickens");
+    alert("Thanks for your review! " + teacherName + "'s average rating is now " + getAvgRating(addTeacherRating) + ".");
   } else {
     prompt("We would like for you to review our teacher. Please enter a rating between 0.0 and 5.0?");
   }
